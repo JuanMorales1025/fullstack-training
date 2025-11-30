1ae154fcfd99081f3b186fb0bc353b9593c53ebd
Juan Morales <jfmv.1025@gmail.com>
2025-11-30 16:57:30 -0500

    Add commentaries

--- python-basics/ejercicio1_crud.py
+++ python-basics/ejercicio1_crud.py
@@ -1,36 +1,38 @@
-tasks = []
-next_id = 1
+tasks = [] #Inicializar una lista donde estaran las tareas almacenadas
+next_id = 1 # Iterador con el que se podra asignar un id a cada tarea 
+						# dentro de la lista
 
-def create_task(title, description):
-    global next_id
-    task = {
-        "id": next_id,
+def create_task(title, description): # Funcion para crear una tarea
+    global next_id # Se toma la variable next id
+    task = {  # Las tareas seran de tipo diccionario 
+        "id": next_id, 
         "title": title,
         "description": description,
-        "done": False
+        "done": False # Toda tarea se inicializa coomo "no hecha"
     }
-    tasks.append(task)
-    next_id += 1
-    return task
+    tasks.append(task) # Se agrega la tarea a la lista 
+    next_id += 1 # Se suma uno mas  al id para la siguiente tarea
+    return task # Se retorna la tarea que fue agregada a la lista (No es necesario) 
 
-def list_tasks():
-    return tasks
+def list_tasks(): # Funcion para visualizar la lista de tareas completa
+    return tasks # Retorna la lista
 
-def update_task(task_id, title=None, description=None, done=None):
-    for task in tasks:
-        if task["id"] == task_id:
-            if title is not None:
-                task["title"] = title
-            if description is not None:
-                task["description"] = description
-            if done is not None:
-                task["done"] = done
-            return task
+def update_task(task_id, title=None, description=None, done=None): # Funcion para editar una tarea 
+    for task in tasks: # Se itera dentro la lista de tareas
+        if task["id"] == task_id: # Comparativo para encontrar la tarea que se quiere editar
+            if title is not None: # Comparativo para saber si queremos editar el titulo
+                task["title"] = title # Se sustituye el titulo
+            if description is not None: # Comparativo para saber si queremos editar la descripcion
+                task["description"] = description # Se sustituye la descripcion
+            if done is not None: # Comparativo para saber si queremos editar el estado de la tarea
+                task["done"] = done # Se sustituye el estado de la tarea
+            return task # Se retorna la tarea con los cambios hechos
     return None
 
-def delete_task(task_id):
-    global tasks
-    tasks = [t for t in tasks if t["id"] != task_id]
+def delete_task(task_id): #Funcion para borrar una tarea de la lista
+    global tasks # Se llama a la lista de tareas
+    tasks = [t for t in tasks if t["id"] != task_id] # Se sustituye la lista de tareas sin incluir la tarea que queremos eliminar
+    
 
 print(create_task("Sacar al perro", "Dar una vuelta"))
 print(create_task("Hacer ejercicio", "Pierna hoy"))
